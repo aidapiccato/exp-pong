92f47a7d75314e74377d4f30ce4b29de3d27e41e
Aida Piccato <aidapiccato@dhcp-10-31-96-224.dyn.mit.edu>
2021-11-19 13:38:15 -0500

    created configs

--- moog_configs/exp_pong_single_ball.py
+++ moog_configs/exp_pong_single_ball.py
@@ -31,23 +31,18 @@ def get_config(_):
     # Sprite initialization
     ############################################################################
 
-    # Occluder
-    occluder_shape = np.array([
-        [-0.1, 0.2], [1.1, 0.2], [1.1, 0.6], [-0.1, 0.6]
-    ])
-    occluder = sprite.Sprite(
-        x=0., y=0., shape=occluder_shape, scale=1., c0=0.6, c1=1., c2=1.)
-    
+
     # Prey 
     prey_factors = distribs.Product(
-        [distribs.Continuous('x', 0.1, 0.8),
-         distribs.Continuous('x_vel', -0.03, 0.03)],
+        [distribs.Discrete('x', candidates=np.linspace(0.1, 0.8, 8)),
+         distribs.Discrete('x_vel', candidates=[-0.01, -0.005, 0.005, 0.01])],
         y=1.2, y_vel=-0.02, shape='circle', scale=0.07, c0=0.2, c1=1., c2=1.,
     )
 
     # Walls
     left_wall = [[0.05, -0.2], [0.05, 2], [-1, 2], [-1, -0.2]]
     right_wall = [[0.95, -0.2], [0.95, 2], [2, 2], [2, -0.2]]
+
     walls = [
         sprite.Sprite(shape=np.array(v), x=0, y=0, c0=0., c1=0., c2=0.5)
         for v in [left_wall, right_wall]
@@ -57,11 +52,30 @@ def get_config(_):
         agent = sprite.Sprite(
             x=0.5, y=0.1, shape='square', aspect_ratio=0.2, scale=0.1, c0=0.33,
             c1=1., c2=0.66)
-        state = collections.OrderedDict([
-            ('walls', walls),
+        # Occluders
+
+        occluder_rad = 0.35
+    
+        occluder_shape_left = np.array([
+            [-1.1, -0.1], [occluder_rad, -0.1], [occluder_rad, 1.1], [-1.1, 1.1]
+        ])
+        occluder_shape_right = np.array([
+            [1 - occluder_rad, -0.1], [2.1, -0.1], [2.1, 1.1], [1 - occluder_rad, 1.1]
+        ])
+
+        opacity = np.random.choice([120, 255], p=[0.3, 0.7])
+ 
+        occluders = [sprite.Sprite(
+            x=0., y=0., shape=occluder_shape_left, scale=1., c0=0., c1=0., c2=0.5, opacity=opacity), 
+                sprite.Sprite(
+            x=0., y=0., shape=occluder_shape_right, scale=1., c0=0., c1=0., c2=0.5, opacity=opacity), 
+        ]
+        
+        state = collections.OrderedDict([            
             ('prey', [sprite.Sprite(**prey_factors.sample())]),
             ('agent', [agent]),
-            ('occluder', [occluder]),
+            ('occluders', occluders),
+            ('walls', walls),
         ])
         return state
 
@@ -73,7 +87,7 @@ def get_config(_):
     asymmetric_collision = physics_lib.Collision(
         elasticity=1., symmetric=False, update_angle_vel=False)
     physics = physics_lib.Physics(
-        (agent_friction_force, 'agent'),
+        (agent_friction_force, ['agent', 'occluders']),
         (asymmetric_collision, 'prey', 'walls'),
         updates_per_env_step=10,
     )
@@ -93,8 +107,14 @@ def get_config(_):
     # Action space
     ############################################################################
 
-    action_space = action_spaces.Joystick(
-        scaling_factor=0.005, action_layers='agent', constrained_lr=True)
+    # action_space = action_spaces.Joystick(
+    #     scaling_factor=0.005, action_layers=['agent', 'occluders'], constrained_lr=True)
+    action_space = action_spaces.Grid(
+        scaling_factor=0.015,
+        action_layers=['agent', 'occluders'],
+        control_velocity=True,
+        momentum=0.5,  
+    )
 
     ############################################################################
     # Observer
